788884cc242f2f31a22aa47c274f17c74e4ddee1
Ashwini Bhujang Pitale <100072536+Ash04P@users.noreply.github.com>
2022-03-13 22:18:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function validate()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if(username=="Ashwini Pitale"&& password=="123456")
+    {
+        alert("login successfully");
+        return false;
+
+    }
+    else
+    {
+        alert("login failed");
+    }
+
+
+
+} 
+  
+
+
+
+
+
+
+
+    
